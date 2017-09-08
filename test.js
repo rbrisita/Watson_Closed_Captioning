d9f30abe30921bc468f7f83fa0b43a2a1bd40bb1
ibmDThompson55 <danlthom@us.ibm.com>
2017-09-08 16:49:19 -0400

    fixed default settings

--- test.js
+++ test.js
@@ -3,10 +3,11 @@ var ts2 = require("./transcript_2").transcript; // two speakers
 
 var formatter = require("./STT_to_CC_Format").toClosedCaption;
 
-var style = {maxLineTime: 3.0, 
-		     includeHesitations: false, 
-		     ccStyle: "srt",
+var style = {maxLineTime: "2.0", 
+		     includeHesitations: true, 
+		     ccStyle: "xrt",
 		     suppressMultipleSpeakers: false};
 
 var s = formatter(ts1, style);
+
 console.log(s);
